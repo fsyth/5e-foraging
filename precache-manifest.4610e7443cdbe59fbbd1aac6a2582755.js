cc0a14009e74847bf31caa8405a3cd64623673b8
James Forsyth <forsythjames@hotmail.co.uk>
2020-10-05 23:36:05 +0100

    Deploy first build

---
+++ precache-manifest.4610e7443cdbe59fbbd1aac6a2582755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ec6c0acbd8c97a1de0a5a570c748cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88f4de0961682a78f562",
+    "url": "/static/css/main.e038e337.chunk.css"
+  },
+  {
+    "revision": "2e4a5cb2e3b97b8ec793",
+    "url": "/static/js/2.663cf0a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.663cf0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f4de0961682a78f562",
+    "url": "/static/js/main.210e61fc.chunk.js"
+  },
+  {
+    "revision": "1b645369e2b15b7304a4",
+    "url": "/static/js/runtime-main.d566223d.js"
+  }
+]);
